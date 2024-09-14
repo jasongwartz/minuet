b9f792949d45cf4595c4a9ebf803d685ca3accdd
Jason Gwartz <jason@gwartz.me>
2024-09-14 15:53:48 +0100

    Load samples from /public/samples in dev server

--- src/lib/load_samples.ts
+++ src/lib/load_samples.ts
@@ -1,23 +1,23 @@
 import * as Tone from 'tone'
 
 export const getSamples = async () => {
-  const metadataResponse = await fetch('https://chopsticks.vercel.app/static/sampledata.json')
-  const metadata = await metadataResponse.json() as Record<string, { file: string, category: string }>
-  return Object.entries(metadata).map((entry) => {
-    return { ...entry[1], name: entry[0] }
-  })
+  const metadataResponse = await fetch('/samples/list')
+  return (await metadataResponse.json()) as { name: string; url: string }[]
 }
 
 export type SampleDetails = Awaited<ReturnType<typeof getSamples>>[number]
 
-export const loadSamples = async (samples: SampleDetails[]) =>  Promise.all(samples.map(async (sample) => {
-  console.log(`loading: ${sample.name}`)
-  const player =  new Tone.Player()
+export const loadSamples = async (samples: SampleDetails[]) =>
+  Promise.all(
+    samples.map(async (sample) => {
+      console.log(`loading: ${sample.name}`)
+      const player = new Tone.Player()
 
-  // TODO: Instead of loading directly from URL, pull binary first,
-  // and read-through cache in IndexedDB (browser storage for blobs)
-  await player.load(`https://chopsticks.vercel.app/${sample.file}`)
-  console.log(`loaded: ${sample.name}`)
+      // TODO: Instead of loading directly from URL, pull binary first,
+      // and read-through cache in IndexedDB (browser storage for blobs)
+      await player.load(sample.url)
+      console.log(`loaded: ${sample.name}`)
 
-  return { ...sample, player }
-}))
+      return { ...sample, player }
+    }),
+  )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+import { join } from 'path'
+import { createReadStream } from 'fs'
+import { readdir } from 'fs/promises'
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'local-samples',
+      async configureServer(server) {
+        const files = await readdir(join(__dirname, 'public/samples'))
+        server.middlewares.use('/samples/list', async (_, res) => {
+          res.end(
+            JSON.stringify(
+              files.map((fp) => ({ name: fp, url: `/samples/${encodeURIComponent(fp)}` })),
+            ),
+          )
+        })
+        server.middlewares.use((req, res, next) => {
+          if (req.url.includes('/samples') && req.url.endsWith('.wav')) {
+            const filePath = join(
+              __dirname,
+              'public/',
+              decodeURIComponent(req.url.match(/.+\/(.*.wav)/)[0]),
+            )
+            console.log('returning sample at:', filePath)
+            const stream = createReadStream(filePath)
+            stream.on('error', next)
+            stream.pipe(res)
+          } else {
+            next()
+          }
+        })
+      },
+    },
+  ],
 })
