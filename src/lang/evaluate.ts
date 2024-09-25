1116ba84dce8e440c818b6a5406693233fa7b639
Jason Gwartz <jason@gwartz.me>
2024-09-25 21:05:19 +0100

    Use "indirect eval" and strict mode (#4)
    * Use "indirect eval" and strict mode
    
    * Fix comment format

--- src/lang/evaluate.ts
+++ src/lang/evaluate.ts
@@ -3,30 +3,37 @@ import ts from 'typescript'
 import type { Engine, OstinatoSchema } from '../ostinato'
 import type { EditorLanguage } from '../ui/Editor'
 
-export const execFromEditor = async (engine: Engine, contents: string, editorLanguage: EditorLanguage) => {
+export const execFromEditor = async (
+  engine: Engine,
+  contents: string,
+  editorLanguage: EditorLanguage,
+) => {
   let evaluatedOutput: OstinatoSchema
 
   const start = Date.now()
   if (editorLanguage === 'pkl') {
-    const response = await
-    // fetch('https://pkl-playground.vercel.app/api/pkl/evaluate', {
+    const response = await // fetch('https://pkl-playground.vercel.app/api/pkl/evaluate', {
     fetch('http://localhost:3000/api/pkl/evaluate', {
       method: 'POST',
-      body: JSON.stringify({ pklInput: contents, outputFormat: 'json' }) ,
+      body: JSON.stringify({ pklInput: contents, outputFormat: 'json' }),
     })
 
     console.log('request took', Date.now() - start)
-    const respJson = await response.json() as unknown
+    const respJson = (await response.json()) as unknown
 
     // TODO: Validate parsed instead of assertion
     evaluatedOutput = JSON.parse((respJson as { output: string }).output) as OstinatoSchema
-
   } else {
     const transpiled = ts.transpile(contents)
     console.log('TypeScript transpilation took', Date.now() - start)
 
     // TODO: Validate parsed instead of assertion
-    evaluatedOutput = eval(transpiled) as OstinatoSchema
+
+    // Use "indirect eval" for a small amount more safety:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval#never_use_direct_eval!
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    evaluatedOutput = eval?.(`"use strict"; ${transpiled}`) as OstinatoSchema
+    console.log('evaluated', evaluatedOutput)
   }
 
   if (Object.keys(engine.samples).length) {
