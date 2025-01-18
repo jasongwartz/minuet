06c2e5df4990150a9f70d75ac5a59f169b09e89a
Jason Gwartz <jason@gwartz.me>
2025-01-18 13:30:39 +0100

    Implement support for synth arpeggiation

--- src/ostinato/index.ts
+++ src/ostinato/index.ts
@@ -54,8 +54,66 @@ export class Engine {
           instrument.synth === 'FMSynth'
             ? new Tone.FMSynth().toDestination()
             : new Tone.AMSynth().toDestination()
-        for (const note of instrument.on.sort()) {
-          synth.triggerAttackRelease('C4', '8n', time + Tone.Time(note).toSeconds())
+
+        for (const playCue of instrument.on.sort((a, b) => {
+          const aBeat = typeof a === 'string' ? a : a.beat
+          const bBeat = typeof b === 'string' ? b : b.beat
+          return Tone.Time(aBeat).toSeconds() - Tone.Time(bBeat).toSeconds()
+        })) {
+          if (typeof playCue === 'string') {
+            synth.triggerAttackRelease('C4', '8n', time + Tone.Time(playCue).toSeconds())
+          } else {
+            if (playCue.mode === 'once') {
+              for (const [index, note] of playCue.notes.entries()) {
+                synth.triggerAttackRelease(
+                  note,
+                  playCue.duration,
+                  time +
+                    Tone.Time(playCue.beat).toSeconds() +
+                    index * Tone.Time(playCue.duration).toSeconds(),
+                )
+              }
+            } else {
+              let timePointer = time
+              let lastNote: string | null = null
+
+              // This function takes a note-string like "C4" and alters the octave
+              // number postfix, so eg `incrementNoteString("C4", 1) returns "C5".
+              const incrementNoteString = (input: string, by: number) => {
+                return input.replace(/(\D+)(\d+)$/, (_, letters: string, number: string) => {
+                  const incrementedNumber = parseInt(number, 10) + by
+                  return letters + incrementedNumber.toString()
+                })
+              }
+
+              const notesWithOctaveVariance = playCue.notes
+                .map((note) => {
+                  const pitches = [note]
+                  for (let i = 1; i < (playCue.octaveVariance ?? 0) + 1; i++) {
+                    pitches.push(incrementNoteString(note, i))
+                    pitches.push(incrementNoteString(note, -i))
+                  }
+                  return pitches.filter((pitch) => Tone.isNote(pitch))
+                })
+                .flat()
+
+              console.log(notesWithOctaveVariance)
+
+              while (timePointer < time + Tone.Time('4m').toSeconds()) {
+                const availableNotes =
+                  lastNote !== null
+                    ? notesWithOctaveVariance.filter((note) => note !== lastNote)
+                    : notesWithOctaveVariance
+
+                const randomIndex = Math.floor(Math.random() * availableNotes.length)
+                // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+                lastNote = availableNotes[randomIndex] as string
+
+                synth.triggerAttackRelease(lastNote, playCue.duration, timePointer)
+                timePointer += Tone.Time(playCue.every).toSeconds()
+              }
+            }
+          }
         }
       } else {
         if (!(instrument.sample.name in this.samples)) {
